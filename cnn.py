f61aad74c6cfa5cb21e407da819759c904a7248c
minusno0708 <naoyao1014@gmail.com>
2021-05-08 14:19:18 +0900

    MNISTデータセットのダウンロードと準備

--- cnn.py
+++ cnn.py
@@ -1,3 +1,11 @@
 import tensorflow as tf
 from tensorflow.keras import datasets, layers, models
 
+
+(train_images, train_labels), (test_images, test_labels) = datasets.mnist.load_data()
+
+train_images = train_images.reshape((60000, 28, 28, 1))
+test_images = test_images.reshape((10000, 28, 28, 1))
+
+# ピクセルの値を 0~1 の間に正規化
+train_images, test_images = train_images / 255.0, test_images / 255.0
