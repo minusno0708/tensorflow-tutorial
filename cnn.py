4d967d284d25b4f0b9ac74a939aacbbe58e80714
minusno0708 <naoyao1014@gmail.com>
2021-05-08 14:36:21 +0900

    CNNを定義

--- cnn.py
+++ cnn.py
@@ -9,3 +9,15 @@ test_images = test_images.reshape((10000, 28, 28, 1))
 
 # ピクセルの値を 0~1 の間に正規化
 train_images, test_images = train_images / 255.0, test_images / 255.0
+
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.Flatten())
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(10, activation='softmax'))
+
+model.summary()
